2d164dbd1d2174312f90d1d4eb862e46770a9223
dstark88 <starkwalker88@gmail.com>
2019-06-12 15:53:11 -0600

    still trying to get json to render

--- foolMeOnce/urls.py
+++ foolMeOnce/urls.py
@@ -18,5 +18,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^', include('materials.urls')),
+    url(r'^articles/', include('articles.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,29 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
 # from django.urls import reverse
 # from django.views import generic
 # from django.template import loader
 import json
 
-
 def index(request):
-    return render(request, 'materials/home.html')
+    with open('./materials/contents_api.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    for content in data['contents']:
+        # print(content['count'], content["next"])
+    # contents =contents.object.all()
+        return render(request, "materials/home.html", {'data':data})
 
 def about(request):
-    return render(request, 'materials/basic.html', {'content':['about me', 'https://dstark88.github.io/portfolio/']})
+    return render(request, 'materials/basic.html', {'content':['about me', 'add a link her']})
 
-# def index(request):
+# def articles(request):
 #     with open("./materials/quotes_api.json", 'r', encoding='utf-8') as json_data:
 #         data = json.load(json_data)
 
 #     # for quote in data['quotes']:
 #     #     print(quote["CompanyName"])
         
-#     # return HttpResponse("Hello, world. You're at the materials index.")
-#     return render(request, 'materials/index.html', {'data':data})
-
-# def article(request):
-#     return render(request, 'materials/article.html')
+#     return render(request, 'articles/articles.html', {'data':data})
 
 # def contentList(request):
 #     with open('./materials/contents_api.json', 'r', encoding='utf-8') as json_file:
