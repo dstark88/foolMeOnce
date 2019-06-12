7bd9ed17db5922030b7a3724ff000c04d26dd9d1
dstark88 <starkwalker88@gmail.com>
2019-06-12 12:32:58 -0600

    learning extends and block

--- materials/urls.py
+++ materials/urls.py
@@ -1,16 +1,7 @@
-from django.urls import path
-from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
-app_name = 'materials'
-
 urlpatterns = [
-    # ex: /materials/
-    # path('', views.index, name='index'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^about/$', views.about),
-    url(r'^article/$', views.article),
-    url(r'^$', views.homepage),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,30 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 # from django.urls import reverse
-from django.views import generic
-from django.template import loader
+# from django.views import generic
+# from django.template import loader
 import json
 
+
 def index(request):
-    with open("./materials/quotes_api.json", 'r', encoding='utf-8') as json_data:
-        data = json.load(json_data)
-
-    # for quote in data['quotes']:
-    #     print(quote["CompanyName"])
-        
-    # return HttpResponse("Hello, world. You're at the materials index.")
-    return render(request, 'materials/index.html', {'data':data})
-
-def homepage(request):
-    return render(request, 'materials/homepage.html')
-    # return HttpResponse('homepage')
+    return render(request, 'materials/home.html')
 
 def about(request):
-    return render(request, 'materials/about.html')
-    # return HttpResponse('about')
+    return render(request, 'materials/basic.html', {'content':['about me', 'https://dstark88.github.io/portfolio/']})
 
-def article(request):
-    return render(request, 'materials/article.html')
+# def index(request):
+#     with open("./materials/quotes_api.json", 'r', encoding='utf-8') as json_data:
+#         data = json.load(json_data)
+
+#     # for quote in data['quotes']:
+#     #     print(quote["CompanyName"])
+        
+#     # return HttpResponse("Hello, world. You're at the materials index.")
+#     return render(request, 'materials/index.html', {'data':data})
+
+# def article(request):
+#     return render(request, 'materials/article.html')
 
 # def contentList(request):
 #     with open('./materials/contents_api.json', 'r', encoding='utf-8') as json_file:
