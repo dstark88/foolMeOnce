f2c5ee48cf79a838f76b1bcf55225d2437e8ce69
dstark88 <starkwalker88@gmail.com>
2019-06-14 15:11:34 -0600

    still trying to parse

--- materials/urls.py
+++ materials/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from . import views
+from django.views.generic import ListView, DetailView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^articles/$', views.articles, name='articles'),
+    # url(r'^articles/(?P<pk>\d+)$'),
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,16 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
-# from django.urls import reverse
-# from django.views import generic
-# from django.template import loader
+
 import json
 
 def index(request):
     with open('./materials/contents_api.json', 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
 
-    for content in data:
+    for result in data['results']:
         # print(content['count'], content["next"])
         return render(request, "materials/home.html", ({'data':data}))
 
